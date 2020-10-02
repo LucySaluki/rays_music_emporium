dd8449412fa206accc3510c9421f7ec91eec2f40
LucySaluki mslucyarnold@gmail.com <mslucyarnold@gmail.com>
2020-10-02 13:23:42 +0100

    added instrument properties

--- src/main/java/Products/Instruments/Instrument.java
+++ src/main/java/Products/Instruments/Instrument.java
@@ -1,4 +1,39 @@
 package Products.Instruments;
 
 public abstract class Instrument {
+    public String material;
+    public String colour;
+    public double boughtPrice;
+    public double sellingPrice;
+
+    public Instrument(String material, String colour, double boughtPrice, double sellingPrice) {
+        this.material = material;
+        this.colour = colour;
+        this.boughtPrice = boughtPrice;
+        this.sellingPrice = sellingPrice;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public double getBoughtPrice() {
+        return boughtPrice;
+    }
+
+    public void setBoughtPrice(double boughtPrice) {
+        this.boughtPrice = boughtPrice;
+    }
+
+    public double getSellingPrice() {
+        return sellingPrice;
+    }
+
+    public void setSellingPrice(double sellingPrice) {
+        this.sellingPrice = sellingPrice;
+    }
 }
